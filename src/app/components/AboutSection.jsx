49220546a991d418ef0f66657007034478fcc3b4
Tyler Farrior <tylerfarrior@gmail.com>
2023-11-30 22:07:38 -0500

    className=rounded-full />

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -56,7 +56,7 @@ const AboutSection = () => {
   return (
     <section className="text-grey-800" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/about-image.png" width={500} height={500} alt="PortfolioHeadshot"/>
+        <Image src="/images/about-image.png" width={500} height={500} alt="PortfolioHeadshot" className="rounded-full" />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-[#6e7f80] mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
