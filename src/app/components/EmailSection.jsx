38a3b96091d1ba16d8311390c86f3d3d8ef0746c
Tyler Farrior <tylerfarrior@gmail.com>
2023-12-31 11:48:28 -0500

    fixing bugs

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 
 const EmailSection = () => {
   const [emailSubmitted, setEmailSubmitted] = useState(false);
+}
 
   const handleSubmit = async (e) => {
     e.preventDefault();
